22680db3ea427dd6b78daad8bb102bd672e748f1
alex farley <37551336+Alex-Farley@users.noreply.github.com>
2026-09-18 23:00:10 +0100

    ci: remove fragile route generator wrapper

--- scripts/generate-routes.mjs
+++
@@ -1,5 +0,0 @@
-import { Generator, getConfig } from "@tanstack/router-generator";
-
-const config = getConfig();
-const generator = new Generator({ config, root: process.cwd() });
-await generator.run();
