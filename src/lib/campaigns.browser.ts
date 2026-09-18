054a98546a50e915e6709466a38118db29d25c22
alex farley <37551336+Alex-Farley@users.noreply.github.com>
2026-09-18 20:23:17 +0100

    Add campaign client wrappers

---
+++ src/lib/campaigns.browser.ts
@@ -0,0 +1,10 @@
+-
+import { createCampaignRecordFn, duplicateCampaignFn, getCampaignFn, listCampaignsFn, saveCampaignAssetFn, updateCampaignRecordFn } from "./campaigns.functions";
+import type { CampaignSummary, CreateCampaignInput, PersistedCampaign, SaveCampaignAssetInput, UpdateCampaignInput } from "./campaigns.functions";
+export type { CampaignSummary, PersistedCampaign, PersistedCampaignAsset } from "./campaigns.functions";
+export const createCampaignRecord=(data:CreateCampaignInput)=>createCampaignRecordFn({data});
+export const saveCampaignAsset=(data:SaveCampaignAssetInput)=>saveCampaignAssetFn({data});
+export const updateCampaignRecord=(data:UpdateCampaignInput)=>updateCampaignRecordFn({data});
+export const listCampaigns=():Promise<CampaignSummary[]>=>listCampaignsFn();
+export const getCampaign=async (campaignId:string):Promise<PersistedCampaign>=>await getCampaignFn({data:{campaignId}}) as PersistedCampaign;
+export const duplicateCampaign=(campaignId:string):Promise<{id:string}>=>(duplicateCampaignFn({data:{campaignId}}));
